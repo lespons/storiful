fde773b00673865b56e579d7ba92e435f8a8df1b
stanislau.dubrouski <stanislau.dubrouski@skelia.com>
2024-04-16 13:18:26 +0200

    feat(ItemStockView): add zebra

--- src/containers/ItemStockView.tsx
+++ src/containers/ItemStockView.tsx
@@ -7,13 +7,14 @@ export async function ItemStockView({
   itemStock: (ItemStock & { ItemType: ItemType })[];
 }) {
   return (
-    <div className="max-h-[80vh] overflow-auto">
+    <div className="max-h-[80vh] flex flex-col">
       <div className="text-lg font-bold">Stock:</div>
-      <div className="flex flex-col gap-0 px-2 py-2 bg-fuchsia-700 bg-opacity-5 rounded-md">
-        {itemStock.map((is) => (
+      <div className="flex flex-col gap-0 px-2 py-4 bg-fuchsia-700 bg-opacity-5 rounded-md overflow-auto">
+        {itemStock.map((is, index) => (
           <div
             key={is.id}
-            className={`flex flex-row gap-4 min-w-full justify-between rounded-md px-4 ${is.value > 0 ? 'text-green-800' : ''}`}>
+            className={`flex flex-row gap-4 min-w-full justify-between px-4 py-1 rounded-sm bg-white ${is.value > 0 ? 'text-green-800' : ''} 
+            ${index % 2 === 0 ? 'bg-opacity-30' : 'bg-opacity-5'}  hover:bg-opacity-50`}>
             <div className="font-bold flex-3">{is.ItemType.name}</div>
             <div className="flex-2">{is.value}</div>
           </div>
