72e42444f75b894a420dfbef3053da5fdc125c97
stanislau.dubrouski <stanislau.dubrouski@skelia.com>
2024-05-23 10:44:57 +0200

    feat(order): generate state pages

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,6 +1,13 @@
 'use server';
 import prisma from '@/lib/prisma';
 import { redirect } from 'next/navigation';
+import { $Enums } from '@prisma/client';
+
+export async function generateStaticParams() {
+  return Object.keys($Enums.OrderStates).map((state) => ({
+    state: state.toLowerCase()
+  }));
+}
 
 export default async function _() {
   const order = await prisma.order.findFirst({
